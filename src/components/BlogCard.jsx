9a47879fcdb916d74a21cf9b8c4520c6a30d55cc
abdulrafay-1 <abdulrock413@gmail.com>
2024-11-14 04:01:04 +0500

    fix blogCard ui image

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -11,15 +11,15 @@ const BlogCard = ({
   return (
     <div className="rounded-md w-full border bg-white border-gray-300 shadow-lg p-3">
       <div className="flex items-center gap-2">
-        <div className="flex items-center justify-center h-[70px] w-[70px] overflow-hidden rounded-md ">
+        <div className="h-[70px] flex-shrink-0 w-[70px] overflow-hidden rounded-md ">
           <img
             src={profile}
             alt=""
-            className="rounded-md h-full w-full object-cover"
+            className="rounded-md  h-full w-full object-cover"
           />
         </div>
-        <div className="flex flex-col flex-grow">
-          <h2 className="font-semibold text-xl w-[80%]">{title}</h2>
+        <div className="">
+          <h2 className="font-semibold text-xl md:w-[85%]">{title}</h2>
           <h2 className="text-gray-500 text-sm">
             {fullName} - {new Date(time).toDateString()}
           </h2>
